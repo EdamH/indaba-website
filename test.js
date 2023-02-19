307e1ee01d237aa8060bccf7d55c7e38cee41558
mrbkd11 <medbkd11@gmail.com>
2023-02-19 20:43:28 +0100

    navbar modifications

--- test.js
+++ test.js
@@ -19,10 +19,47 @@ document.querySelectorAll(".menu-link").forEach((link) => {
 });
 
 // Change Header Background on Scrolling
-window.addEventListener("scroll", () => {
-   if (this.scrollY >= 85) {
-      headerMenu.classList.add("on-scroll");
-   } else {
-      headerMenu.classList.remove("on-scroll");
-   }
-});
+// window.addEventListener("scroll", () => {
+//    if (this.scrollY >= 85) {
+//       headerMenu.classList.add("on-scroll");
+//    } else {
+//       headerMenu.classList.remove("on-scroll");
+//    }
+// });
+
+
+
+
+
+if (!navbarMenu.classList.contains('is-active')) {
+   var header = document.querySelector('.header');
+
+   ScrollTrigger.create({
+      start: 'top -50',
+      end: 99999,
+      markers: false,
+      toggleClass: { className: 'jwpnavbar--scrolled', targets: '.header' }
+   });
+
+   ScrollTrigger.create({
+      start: 'top -300',
+      end: 99999,
+      toggleClass: { className: 'jwpnavbar--up', targets: '.header' },
+      onUpdate: ({ direction }) => {
+         if (direction == -1) {
+            header.classList.remove('jwpnavbar--up');
+         } else {
+            header.classList.add('jwpnavbar--up');
+         }
+      }
+   });
+
+   gsap.to('.header', {
+      color: '#fff',
+      scrollTrigger: {
+         trigger: '.header',
+         start: 'top -150',
+         scrub: true
+      }
+   });
+}
