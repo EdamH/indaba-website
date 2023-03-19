d859788a1327a100dac879a536d3fde0c04ed667
unknown <raed.rokh@supcom.tn>
2023-03-20 00:04:17 +0100

    fixed bugs- parallax remaining

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,41 @@
 
+
+
+
+
+
+// Get all the speaker containers
+const speakerContainers = document.querySelectorAll('.mu-single-speakers');
+console.log(speakerContainers);
+// Add a hover event listener to each container
+speakerContainers.forEach(container => {
+	container.addEventListener('mouseenter', () => {
+	
+		const imgcontainer = container.querySelector('.speaker-img');
+		// Get the info container for this speaker
+		const infoContainer = container.querySelector('.mu-single-speakers-info');
+
+		// Apply the transition effect
+		imgcontainer.style.filter = 'blur(10px)';
+		infoContainer.style.transform = 'translateY(-120%)';
+		infoContainer.style.display = 'block';
+		infoContainer.style.opacity = '1';
+	});
+
+	container.addEventListener('mouseleave', () => {
+		// Get the info container for this speaker
+		const infoContainer = container.querySelector('.mu-single-speakers-info');
+		const imgcontainer = container.querySelector('.speaker-img');
+		// Remove the transition effect
+
+		// infoContainer.style.display = 'none';
+		infoContainer.style.transform = 'translateY(-160%)';
+		imgcontainer.style.filter = 'blur(0px)';
+		infoContainer.style.opacity = '0';
+
+	});
+});
+
 $('.mu-speakers-slider').slick({
 	slidesToShow: 4,
 	responsive: [
@@ -31,39 +68,4 @@ $('.mu-speakers-slider').slick({
 			}
 		}
 	]
-});
-
-
-
-
-
-// Get all the speaker containers
-const speakerContainers = document.querySelectorAll('.mu-single-speakers');
-
-// Add a hover event listener to each container
-speakerContainers.forEach(container => {
-	container.addEventListener('mouseenter', () => {
-		const imgcontainer = container.querySelector('.speaker-img');
-		// Get the info container for this speaker
-		const infoContainer = container.querySelector('.mu-single-speakers-info');
-
-		// Apply the transition effect
-		imgcontainer.style.filter = 'blur(2px)';
-		infoContainer.style.transform = 'translateY(-120%)';
-		infoContainer.style.display = 'block';
-		infoContainer.style.opacity = '1';
-	});
-
-	container.addEventListener('mouseleave', () => {
-		// Get the info container for this speaker
-		const infoContainer = container.querySelector('.mu-single-speakers-info');
-		const imgcontainer = container.querySelector('.speaker-img');
-		// Remove the transition effect
-
-		// infoContainer.style.display = 'none';
-		infoContainer.style.transform = 'translateY(-160%)';
-		imgcontainer.style.filter = 'blur(0px)';
-		infoContainer.style.opacity = '0';
-
-	});
 });
