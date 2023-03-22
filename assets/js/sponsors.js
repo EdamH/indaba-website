cdbb838326a105cc08113f22ba1d4f9760278d06
AzizBoussarsar <aziz.bou0510@gmail.com>
2023-03-22 09:18:03 +0100

    section done responsiveness remaining

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -12,3 +12,16 @@ control.addEventListener("click", () => {
     marquee.classList.toggle("marquee--vertical")
   );
 });
+
+
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
