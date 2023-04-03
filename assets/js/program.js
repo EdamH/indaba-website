88449fac23c0e030f9813297678a6cc132cd57f4
RaedRokh <raed.rokh@supcom.tn>
2023-04-03 21:07:31 +0100

    fix animation & responsiveness issue on program section

--- assets/js/program.js
+++ assets/js/program.js
@@ -27,14 +27,17 @@ const isTouchDevice = () => {
         vwRestricted = 1920 / 100;
     }
 let scrollTimelineEnd = 'bottom 0%';
+let scrollTimelineEnd2 = 'bottom 0%';
 
 if (navigator.userAgent.match(/firefox|fxios/i)) {
     // scrollTimelineEnd = 'top -8000px';
-    scrollTimelineEnd = '% 0%';
+    scrollTimelineEnd = '60% 0%';
+    scrollTimelineEnd2 = '150% 0%';
 }
 
 if (windowWidth < 850 || isTouchDevice()) {
-    scrollTimelineEnd = '50% 0%';
+    scrollTimelineEnd = '60% 0%';
+    scrollTimelineEnd2 = '150% 0%';
 }
   const timeline = gsap.timeline({
               scrollTrigger: {
@@ -61,5 +64,24 @@ if (windowWidth < 850 || isTouchDevice()) {
     .to('.section-smoother .inner-container .flex-container-6', {duration: .2, delay: .3, x: 0, ease: 'linear'}, 'smoother-section-animation+=.2')
     // .from('.section-smoother .content', {duration: .8, y: 110 * vh, ease: 'linear'}, 'smoother-section-animation+=.2')
 
-  
+    const timeline1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.inner-container1',
+            start: '0% 0%',
+            end: scrollTimelineEnd2,
+            scrub: 1,
+        
+        }
+    });
+    timeline1
+    .add('smoother-section-animation')
+      // .to('.section-smoother .content', {duration: 1.2, y: -110 * vh, ease: 'linear'}, 'smoother-section-animation+=.2')
+      .to('.section-smoother .inner-container1', {duration: 1.9, y: '-10vh', ease: 'linear'}, 'smoother-section-animation+=.2')
+      .fromTo('.section-smoother .inner-container1 .flex-container-2', {duration: .3, x: '6rem', ease: 'linear'}, {duration: .3, x: '2rem', ease: 'linear'}, 'smoother-section-animation+=.2')
+      .fromTo('.section-smoother .inner-container1 .flex-container-3', {duration: .6, x: '10rem', ease: 'linear'}, {duration: .3, x: '2rem', ease: 'linear'}, 'smoother-section-animation+=.2')
+        .fromTo('.section-smoother .inner-container1 .flex-container-4', { duration: .9, x: '14rem', ease: 'linear' }, { duration: .3, x: '2rem', ease: 'linear' }, 'smoother-section-animation+=.2')
+        .to('.section-smoother .inner-container1 .flex-container-2', {duration: .2, delay: .3, x: 0, ease: 'linear'}, 'smoother-section-animation+=.2')
+        .to('.section-smoother .inner-container1 .flex-container-3', {duration: .2, delay: .3, x: 0, ease: 'linear'}, 'smoother-section-animation+=.2')
+      .to('.section-smoother .inner-container1 .flex-container-4', {duration: .2, delay: .3, x: 0, ease: 'linear'}, 'smoother-section-animation+=.2')
+
   
