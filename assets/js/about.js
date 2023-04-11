c7c24a63c4570420c1d3515395e4b713a057251b
EdamH <edam.hamza@supcom.tn>
2023-04-11 21:13:53 +0100

    Elongated animation again

--- assets/js/about.js
+++ assets/js/about.js
@@ -29,17 +29,17 @@ function LandingPageScrollTrigger() {
     // .to('#ImgWrapper #img8', { transform: 'translateZ(0px)', }, 15)
   LandingPageScrollTrigger
     .to('#ImgWrapper #img8', { transform: 'translateZ(2200px)', }, 0)
-      .from('#codeby a', { y: 130, opacity: 0 }, 0.31)
-        .to('#codeby a', { y: 0, opacity: 0 }, 3)
-      .from('#codeby #test', { y: 130, opacity: 0 }, 4)
-        .to('#codeby #test', { y: 0, opacity: 0 }, 7)
-      .from('#codeby #test1', { y: 130, opacity: 0 }, 8)
-        .to('#codeby #test1', { y: 0, opacity: 0 }, 11)
-      .from('#codeby #test2', { y: 130, opacity: 0 }, 12)
-        .to('#codeby #test2', { y: 0, opacity: 0 }, 15)
-      .from('#codeby #test3', { y: 130, opacity: 0 }, 16)
-        .to('#codeby #test3', { y: 0, opacity: 0 }, 19)
-      .to('#ImgWrapper #img8', { transform: 'translateZ(0px)', }, 20)
+      .from('#codeby a', { y: 130, opacity: 0 }, 1)
+        .to('#codeby a', { y: 0, opacity: 0 }, 4)
+      .from('#codeby #test', { y: 130, opacity: 0 }, 6)
+        .to('#codeby #test', { y: 0, opacity: 0 }, 9)
+      .from('#codeby #test1', { y: 130, opacity: 0 }, 11)
+        .to('#codeby #test1', { y: 0, opacity: 0 }, 14)
+      .from('#codeby #test2', { y: 130, opacity: 0 }, 16)
+        .to('#codeby #test2', { y: 0, opacity: 0 }, 19)
+      .from('#codeby #test3', { y: 130, opacity: 0 }, 21)
+        .to('#codeby #test3', { y: 0, opacity: 0 }, 24)
+      .to('#ImgWrapper #img8', { transform: 'translateZ(0px)', }, 26)
 
 };
         
